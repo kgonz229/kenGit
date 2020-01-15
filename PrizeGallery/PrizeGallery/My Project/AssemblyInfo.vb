f5aa337a87025da7c43af3a7612bf19a51dc2f1f
kgonz229 <kgonz229@fiu.edu>
2020-01-14 21:43:27 -0500

    Commit

---
+++ PrizeGallery/PrizeGallery/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PrizeGallery")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PrizeGallery")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3510d08a-a864-4a71-b813-aaa5c72284eb")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ PrizeGallery/PrizeGallery/frmMain.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblPrizeVal = New System.Windows.Forms.Label()
+        Me.lblWinnings = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblForfeit = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.picPrize = New System.Windows.Forms.PictureBox()
+        Me.timSpin = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picPrize, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(77, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(871, 64)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Panther Prize Gallery - Spin to Win Prizes"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(84, 101)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(165, 57)
+        Me.lblResult.TabIndex = 1
+        Me.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblPrizeVal
+        '
+        Me.lblPrizeVal.BackColor = System.Drawing.Color.White
+        Me.lblPrizeVal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPrizeVal.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrizeVal.Location = New System.Drawing.Point(84, 201)
+        Me.lblPrizeVal.Name = "lblPrizeVal"
+        Me.lblPrizeVal.Size = New System.Drawing.Size(165, 57)
+        Me.lblPrizeVal.TabIndex = 2
+        Me.lblPrizeVal.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblWinnings
+        '
+        Me.lblWinnings.BackColor = System.Drawing.Color.White
+        Me.lblWinnings.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblWinnings.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWinnings.Location = New System.Drawing.Point(84, 299)
+        Me.lblWinnings.Name = "lblWinnings"
+        Me.lblWinnings.Size = New System.Drawing.Size(165, 57)
+        Me.lblWinnings.TabIndex = 3
+        Me.lblWinnings.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(80, 168)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(93, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Prize Value:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(80, 268)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(155, 20)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Your Total Winnings:"
+        '
+        'lblForfeit
+        '
+        Me.lblForfeit.AutoSize = True
+        Me.lblForfeit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblForfeit.ForeColor = System.Drawing.Color.Red
+        Me.lblForfeit.Location = New System.Drawing.Point(92, 382)
+        Me.lblForfeit.Name = "lblForfeit"
+        Me.lblForfeit.Size = New System.Drawing.Size(152, 32)
+        Me.lblForfeit.TabIndex = 6
+        Me.lblForfeit.Text = "You Lose!"
+        Me.lblForfeit.Visible = False
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(84, 458)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(164, 53)
+        Me.btnStart.TabIndex = 7
+        Me.btnStart.Text = "Start the Spin"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(84, 639)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(164, 53)
+        Me.btnQuit.TabIndex = 8
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(84, 550)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(164, 53)
+        Me.btnStop.TabIndex = 9
+        Me.btnStop.Text = "Stop the Spin"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'picPrize
+        '
+        Me.picPrize.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.picPrize.Image = Global.PrizeGallery.My.Resources.Resources.prize_1
+        Me.picPrize.Location = New System.Drawing.Point(321, 104)
+        Me.picPrize.Name = "picPrize"
+        Me.picPrize.Size = New System.Drawing.Size(174, 263)
+        Me.picPrize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.picPrize.TabIndex = 10
+        Me.picPrize.TabStop = False
+        '
+        'timSpin
+        '
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Linen
+        Me.ClientSize = New System.Drawing.Size(1031, 751)
+        Me.Controls.Add(Me.picPrize)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.lblForfeit)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblWinnings)
+        Me.Controls.Add(Me.lblPrizeVal)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mystery Prizes"
+        CType(Me.picPrize, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents lblPrizeVal As Label
+    Friend WithEvents lblWinnings As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblForfeit As Label
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents picPrize As PictureBox
+    Friend WithEvents timSpin As Timer
+End Class
